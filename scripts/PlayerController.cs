27bd6744ef5694c48ed141230375650c085c3b9d
Ilkka Wahala <ilkka.wahala@tuni.fi>
2025-06-25 14:56:15 +0300

    Feat: improve player movement and add placeholder footstep sound effect

--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -5,6 +5,10 @@ namespace dungeonCrawler
 {
 	public partial class PlayerController : Node3D
 	{
+		[Export] private float _movementSpeed = 6f;
+
+		[Export] private AudioStreamPlayer3D _footstepPlayer;
+
 		[Export] private RayCast3D _frontRay;
 		[Export] private RayCast3D _leftRay;
 		[Export] private RayCast3D _rightRay;
@@ -12,13 +16,18 @@ namespace dungeonCrawler
 
 		private Tween tween;
 
-		[Export] private float _movementSpeed = 6f;
 		private float _travelDistance = 2f;
 		private float _travelTime;
 
 		public override void _Ready()
 		{
 			_travelTime = _travelDistance / _movementSpeed;
+
+			if (_footstepPlayer
+ == null)
+			{
+				GD.PrintErr("Sound effect player is not assigned!");
+			}
 		}
 
 		public override void _PhysicsProcess(double delta)
@@ -26,66 +35,71 @@ namespace dungeonCrawler
 			if (tween != null && tween.IsRunning())
 				return;
 
-			// Move forward in the direction the player is facing
 			if (Input.IsActionPressed("MoveForward") && !_frontRay.IsColliding())
 			{
-				tween = CreateTween();
-				Vector3 moveDir = -Transform.Basis.Z.Normalized();
-				tween.TweenProperty(this, "transform",
-					Transform.Translated(moveDir * _travelDistance), _travelTime);
+				MoveInDirection(-Transform.Basis.Z.Normalized());
 			}
 
-			// Move left in relation to the direction the player is facing
 			if (Input.IsActionPressed("MoveLeft") && !_leftRay.IsColliding())
 			{
-				tween = CreateTween();
-				Vector3 moveDir = -Transform.Basis.X.Normalized();
-				tween.TweenProperty(this, "transform",
-					Transform.Translated(moveDir * _travelDistance), _travelTime);
+				MoveInDirection(-Transform.Basis.X.Normalized());
 			}
 
-			// Move right in relation to the direction the player is facing
 			if (Input.IsActionPressed("MoveRight") && !_rightRay.IsColliding())
 			{
-				tween = CreateTween();
-				Vector3 moveDir = Transform.Basis.X.Normalized();
-				tween.TweenProperty(this, "transform",
-					Transform.Translated(moveDir * _travelDistance), _travelTime);
+				MoveInDirection(Transform.Basis.X.Normalized());
 			}
 
-			// Move backward (opposite of current facing)
 			if (Input.IsActionPressed("MoveBackward") && !_backRay.IsColliding())
 			{
-				tween = CreateTween();
-				Vector3 moveDir = Transform.Basis.Z.Normalized();
-				tween.TweenProperty(this, "transform",
-					Transform.Translated(moveDir * _travelDistance), _travelTime);
+				MoveInDirection(Transform.Basis.Z.Normalized());
 			}
 
-			// Turn left (90° counter-clockwise)
 			if (Input.IsActionPressed("TurnLeft"))
 			{
-				tween = CreateTween();
-				Basis rotated = Transform.Basis.Rotated(Vector3.Up, Mathf.Pi / 2);
-				Transform3D newTransform = Transform;
-				newTransform.Basis = rotated;
-				tween.TweenProperty(this, "transform", newTransform, _travelTime);
+				RotateByAngle(Mathf.Pi / 2);
 			}
 
-			// Turn right (90° clockwise)
 			if (Input.IsActionPressed("TurnRight"))
 			{
-				tween = CreateTween();
-				Basis rotated = Transform.Basis.Rotated(Vector3.Up, -Mathf.Pi / 2);
-				Transform3D newTransform = Transform;
-				newTransform.Basis = rotated;
-				tween.TweenProperty(this, "transform", newTransform, _travelTime);
+				RotateByAngle(-Mathf.Pi / 2);
 			}
 
 			if (Input.IsActionPressed("Quit"))
 			{
 				GetTree().Quit();
 			}
+		}
+
+		private void MoveInDirection(Vector3 direction)
+		{
+			tween = CreateTween();
+			tween.TweenProperty(this, "transform",
+				Transform.Translated(direction * _travelDistance), _travelTime);
+
+			tween.Parallel()
+				.TweenCallback(Callable.From(() => PlayFootstep()))
+				.SetDelay(_travelTime / 1.5f);
+		}
+
+		private void RotateByAngle(float angle)
+		{
+			tween = CreateTween();
+			Basis rotated = Transform.Basis.Rotated(Vector3.Up, angle);
+			Transform3D newTransform = Transform;
+			newTransform.Basis = rotated;
+			tween.TweenProperty(this, "transform", newTransform, _travelTime);
+
+			//tween.Parallel()
+			//	.TweenCallback(Callable.From(() => PlayFootstep()))
+			//	.SetDelay(_travelTime / 1.5f);
+		}
+
+		private void PlayFootstep()
+		{
+			_footstepPlayer.VolumeDb = (float)GD.RandRange(0f, -5f);
+			_footstepPlayer.PitchScale = (float)GD.RandRange(0.5f, 0.75f);
+			_footstepPlayer.Play();
 		}
 	}
 }
