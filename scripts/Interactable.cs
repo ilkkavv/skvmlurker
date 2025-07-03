73b4a3384c44128d0b12c3bca0bb22cd291a5536
unknown <ilkka.wahala@tuni.fi>
2025-07-03 11:17:29 +0300

    docs: Add detailed comments and summaries to Interactable script

--- scripts/Interactable.cs
+++ scripts/Interactable.cs
@@ -2,8 +2,20 @@ using Godot;
 
 namespace dungeonCrawler
 {
+	/// <summary>
+	/// Base class for interactable objects in the dungeon.
+	/// Inherit from this and implement <see cref="OnInteract"/> to define custom interaction logic.
+	/// </summary>
 	public abstract partial class Interactable : StaticBody3D
 	{
+		#region Interaction
+
+		/// <summary>
+		/// Called when the player interacts with this object.
+		/// Must be implemented by derived classes to define custom behavior.
+		/// </summary>
 		public abstract void OnInteract();
+
+		#endregion
 	}
 }
